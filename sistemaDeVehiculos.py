9e67f4c124583058c99c4703d404f5616a53d64e
jlopezsa2 <118971769+jlopezsa2@users.noreply.github.com>
2025-02-06 13:39:40 +0100

    Add files via upload

---
+++ sistemaDeVehiculos.py
@@ -0,0 +1,14 @@
+class Vehiculo:
+    def __init__(self, marca, modelo, año):
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
+
+    def arrancar(self):
+        print('El vehiculo esta arrancando...')
+        
+    def detener(self):
+        print('El vehiculo se ha detenido...')   
+        
+        
+        
